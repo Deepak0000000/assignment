22a553f689e143c824e38e312cf3e85adae18851
Deepak Singh <104123364+Deepak0000000@users.noreply.github.com>
2023-03-02 13:31:53 +0530

    Add files via upload

---
+++ 18th feb assignment.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# aANS1
+
+# In[1]:
+
+
+"""An application programming interface (API) is the medium by which different software interact.
+They foster connections between technologies to enhance user experience.
+
+EXAMPLE:
+
+Google Maps
+The Google Maps API gives users the privilege of nearly limitless geographic aptitude at their fingertips. Search nearby restaurants, niche shops, and whatever else is in relative distance to your location.
+
+You may have been using this API example more often than you realize. Each time you glimpsed business hours, reviews, contact information, or anything of that nature from that handy box on your screen, that is the Google Maps API in action.
+
+To that same effect, clicking on the map icon in that box will open the Google Maps app for you or take you straight to the Google Maps website.
+"""
+
+
+# ANS2
+
+# In[2]:
+
+
+"""
+API advantages:
+
+Can provide a way to access functionality or data that is not otherwise readily available or accessible.
+Can provide a way to automate tasks.
+Can help to improve communication between disparate systems.
+
+API disadvantages:
+
+Can be difficult to use and understand.
+Cannot be guaranteed to be stable or backwards compatible.
+May require a certain level of technical expertise in order to use them effectively"""
+
+
+# ANS3
+
+# In[3]:
+
+
+"""What is REST API?
+Before we jump right into it, let’s first understand what REST is. 
+Contrary to the belief of many, REST is not a protocol, a tool or library, 
+but rather an architectural style of web service that provides a channel of 
+communication between systems or computers on the internet. It is a standard that
+is utilized as an architectural means of designing a Network-based software system.
+
+Examples of REST APIs
+Instagram API permits your applications to retrieve user tags, photos, account and much more.
+Twitter also provides a REST API which a developer can query to source the latest tweets, or 
+provide a search query that will return the results in JSON format.
+
+What is Web API
+Web API is basically an open-source framework that is used to write HTTP APIs. It refers to an API over the web which can be accessed using the HTTP protocol. It is important to note that it is a concept and not a technology. Developers can build Web API using a vast array of technologies such as .NET, and Java, among others. Web API can be RESTful or not. Web API implements protocol specification and thus it incorporates concepts like caching, URIs, versioning, request/response headers, and various content formats in it.
+
+Examples of Web APIs
+Google APIs – In any area of modern technology, you can be assured that Google will set the benchmark. Their APIs include Google Analytic API, YouTube API, Blogger API, and Google Font API, among others.
+There are also social media APIs such as Twitter API, Facebook APIs, Scoop.it API and Linkedin API.
+
+"""
+
+
+# In[4]:
+
+
+"""SOAP API:-
+SOAP (Simple Access Protocol) is a standard messaging or communication protocol system that allows processes
+that utilize various operating systems such as Windows and Linux to interact and communicate through HTTP and its XML.
+SOAP APIs are designed with the capability to create, update, recover and delete records such as passwords, leads, accounts, 
+and custom objects. While Web API in the time of Web 1.0 was synonymous with SOAP-based web services, today in Web 2.0, 
+the term SOAP is edging towards REST-style web resources."""
+
+
+# ANS5
+
+# In[5]:
+
+
+"""  
+SOAP API
+
+                                                                                                REST API
+
+Relies on SOAP (Simple Object Access Protocol)	                  Relies on REST (Representational State Transfer) architecture using HTTP.
+Transports data in standard XML format.	                           Generally transports data in JSON. It is based on URI. Because REST follows stateless model, REST does not enforces message format as XML or JSON etc.
+Because it is XML based and relies on SOAP, 
+it works with WSDL	                                               It works with GET, POST, PUT, DELETE
+Works over HTTP, HTTPS, SMTP, XMPP	                               Works over HTTP and HTTPS
+Highly structured/typed	                                           Less structured -> less bulky data
+Designed with large enterprise applications in mind              	Designed with mobile devices in mind"""
+
+
+# In[ ]:
+
+
+
+
