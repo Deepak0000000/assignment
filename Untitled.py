909cd5b95d69736311330b6b90de9dc5dc551a84
Deepak Singh <104123364+Deepak0000000@users.noreply.github.com>
2023-03-20 10:24:04 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ANS1
+
+# In[1]:
+
+
+"""There are two main types of statistics: descriptive statistics and inferential statistics.
+
+Descriptive statistics: These statistics are used to describe or summarize a set of data. They are used to organize, 
+summarize and describe the characteristics of the data.
+Example: A company wants to know the average age of its employees. 
+The company collects data on the ages of its employees and uses descriptive statistics to summarize the data, 
+finding that the average age of the employees is 35 years.
+
+Inferential statistics: These statistics are used to draw conclusions about a population based on a sample of data. 
+Inferential statistics are used to make generalizations about a population based on a sample.
+Example: A company wants to know if there is a difference in the average salary of male and female employees. 
+The company collects data on the salaries of a sample of male and female employees and uses inferential statistics to 
+determine if there is a significant difference between the two groups."""
+
+
+# ANS2
+
+# In[2]:
+
+
+"""There are four main types of data: nominal, ordinal, interval, and ratio data. Each type of data is defined by the properties of the values that are measured or observed.
+
+Nominal data: This type of data is used to categorize or name things. The values are not ordered, and there is no inherent numerical relationship between them.
+Example: The colors of cars in a parking lot.
+
+Ordinal data: This type of data has an inherent order, but the difference between the values is not meaningful or equal.
+Example: The rankings of runners in a race.
+
+Interval data: This type of data has an inherent order and the difference between values is meaningful and equal, but there is no true zero point.
+Example: Temperature measured in degrees Celsius.
+
+Ratio data: This type of data has an inherent order, the difference between values is meaningful and equal, and there is a true zero point.
+Example: Height measured in centimeters."""
+
+
+# ANS3
+
+# In[3]:
+
+
+"""i Grading in exam: This is an example of qualitative data as the grades are categories and not numerical values.
+
+ii Colour of mangoes: This is an example of qualitative data as the colors are categories and not numerical values.
+
+iii Height data of a class: This is an example of quantitative data as the values are numerical.
+
+iv Number of mangoes exported by a farm: This is an example of quantitative data as the values are numerical."""
+
+
+# ANS4
+
+# In[4]:
+
+
+""" There are four main levels of measurement: nominal, ordinal, interval, and ratio. 
+
+Nominal: This is the lowest level of measurement and is used for categorical data where values represent categories or names. Data at this level cannot be ordered or compared using numerical methods.
+Example: Gender (male or female), Hair color (black, brown, blonde, etc.)
+
+Ordinal: This level of measurement is used for data that can be ranked or ordered, but the differences between values may not be meaningful.
+Example: Educational level (elementary, high school, college), Rank in a competition (first, second, third, etc.)
+
+Interval: This level of measurement is used for data that can be ordered and the difference between values is meaningful. However, there is no true zero point.
+Example: Temperature measured in Celsius or Fahrenheit, Dates on a calendar.
+
+Ratio: This level of measurement is used for data that can be ordered, the difference between values is meaningful, and there is a true zero point.
+"""
+
+
+# ANS5
+
+# In[7]:
+
+
+"""It is important to understand the level of measurement when analyzing data because the level of measurement determines the type of statistical analysis that can be applied to the data. Different statistical tests are appropriate for different levels of measurement. Using an inappropriate statistical test can lead to incorrect conclusions and can invalidate the results.
+
+For example, if we have a variable that measures the income of individuals in a population, we need to know the level of measurement of this variable before we can apply statistical analyses. If the income variable is nominal or ordinal, we cannot use tests like t-tests or ANOVA, which require interval or ratio data."""
+
+
+# ANS6
+
+# In[8]:
+
+
+"""Nominal data is the lowest level of measurement and is used for data that can be classified into categories or names. 
+The values of nominal data cannot be ranked or ordered, and there is no inherent numerical relationship between the values.
+Examples of nominal data include gender (male, female), hair color (black, brown, blonde), 
+and types of fruit (apple, banana, orange).
+
+Ordinal data is used for data that can be ranked or ordered, but the differences between the values are not necessarily 
+equal or meaningful. The values of ordinal data can be categorized, and they have an inherent order, but the numerical 
+relationship between them is not constant. Examples of ordinal data include rankings in a competition (first, second, third),
+educational level (elementary, high school, college), or a rating system (strongly agree, agree, neutral, disagree, strongly
+disagree)."""
+
+
+# ANS8
+
+# In[9]:
+
+
+"""The box of the plot represents the interquartile range (IQR), which is the range of values from the 
+first quartile (Q1) to the third quartile (Q3). The line within the box represents the median, and the 
+whiskers extend to the minimum and maximum values within a specified range. Any data points outside of
+the whiskers are considered outliers and are displayed as individual points.
+
+By displaying the range of the data in a visual format, the box plot allows us to easily compare multiple datasets
+and identify any potential outliers or patterns in the data. It is particularly useful for comparing datasets with 
+different distributions or for identifying any skewness or asymmetry in the data."""
+
+
+# ANS7
+
+# In[10]:
+
+
+"""Descriptive and inferential statistics are two broad categories of statistical analysis used in research and data analysis.
+
+Descriptive statistics are used to describe and summarize data using measures such as mean, median, mode, range, and 
+standard deviation.
+Descriptive statistics provide a clear and concise summary of the data, and are useful for presenting the data in a way
+that is easily understandable. They can also be used to identify patterns, trends, or outliers in the data. For example,
+the average height of students in a class, the number of cars sold in a month, or the distribution of ages in a population 
+are all examples of descriptive statistics.
+
+Inferential statistics, on the other hand, are used to draw conclusions or make inferences about a population based on
+a sample of data. Inferential statistics involve using probability theory and hypothesis testing to make generalizations
+about the population. They are used to test hypotheses, make predictions, and estimate parameters of interest. For example, 
+testing whether a new drug is effective in treating a disease, determining if there is a significant difference between the
+mean incomes of two groups, or predicting the number of voters who will support a political candidate are all examples of 
+inferential statistics."""
+
+
+# ANS9
+
+# In[ ]:
+
+
+"""Mean: The mean is the arithmetic average of a dataset and is calculated by summing all values in the dataset and dividing by the number of values. The mean is a useful measure of central tendency for datasets that are normally distributed and have no outliers.
+
+Median: The median is the middle value of a dataset and is calculated by arranging the values in ascending order and selecting the middle value. The median is a useful measure of central tendency for datasets that have outliers or are skewed.
+
+Mode: The mode is the value that occurs most frequently in a dataset. The mode is a useful measure of central tendency for datasets with categorical or nominal data.
+
+Range: The range is the difference between the maximum and minimum values in a dataset. The range is a useful measure of variability as it provides a quick summary of the spread of the data.
+
+Variance: The variance is a measure of how spread out a dataset is from the mean. It is calculated by summing the squared differences between each value and the mean, and dividing by the number of values. A larger variance indicates a more spread out dataset, while a smaller variance indicates a more tightly clustered dataset.
+
+Standard deviation: The standard deviation is the square root of the variance and is another measure of how spread out a dataset is from the mean. The standard deviation is a useful measure of variability as it is in the same units as the data and is easily interpretable."""
+
